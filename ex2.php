0bc12e34b896a5a2a393ccd8df3779e016bf0f5f
thomas desiano <21905417@etu.unicaen.fr>
2020-05-05 17:40:53 +0200

    bon ex2

--- ex2.php
+++ ex2.php
@@ -1,42 +1,56 @@
-<?php
+<!doctype html>
+<html lang="fr">
+<head>
+	<title>TP1 - Exo2</title>
+	<meta charset="utf-8">
+	<meta name="description" content="ex2">
 
+</head>
 
-$message = $_GET["message"];
+<body>
 
-$size = $_GET["size"];
+	<h2><a href="./ex2.php?message=Bonjour&size=25&color=%23ff0000">Afficher "Bonjour" en rouge, avec une taille de 25px</a></h2>
+	<h2><a href="./ex2.php?message=Bonjour&size=50&color=%232ff000">Afficher "Bonjour" en vert, avec une taille de 50px</a></h2>
+	<h2><a href="./ex2.php?message=Bonjour&size=100&color=%230000ff">Afficher "Bonjour" en bleu, avec une taille de 100px</a></h2>
+	<p>
+		<?php 
+			$size = 12;
+			$color = 0;
+			$message = "Aucun message";
 
-$color = $_GET["color"];
+			if(!empty($_GET['size']))$size=$_GET['size'];
+			if(!empty($_GET['color']))$color=urldecode($_GET['color']);
+			if(!empty($_GET['message']))$message=urldecode($_GET['message']);
 
+		?>
+		
+		<?php if(!empty($_GET)) { ?>
 
-$msg .= '<div style="font-size: '. $size .'px;color:'.$color.'">'. $message .'</div>';
+			<div style="font-size: <?=$size?>px;color:<?=$color?>"><?=$message?></div>
+		<?php }else{ ?>
+			<div style="font-size: 50px;color: #ff0000;">Bonjour</div>
 
-$form .= '
-<form method="GET">
-    <label for="message"> Message : </label>
-    <input type="text" value="" name="message" id="message">
-    <label for="size">Size : </label>
-    <input type="number" value="10" name="size" id="size">
-    <label for="color">Color :  </label>
-    <input type="color" value="" name="color" id="color">
-    <input type="submit" value="Valider">
-</form>';
+		<?php } ?>
+	</p>
+	<h3>
+  		<a href="./ex2.php?message=<?=urlencode($message)?>&size=<?=$size+1?>&color=<?=urlencode($color)?>"><button>+</button></button></a>  
+  		<a href="./ex2.php?message=<?=urlencode($message)?>&size=<?=$size-1?>&color=<?=urlencode($color)?>"><button>-</button></a>
 
-$link .= '
-<a href="/web-dyna-alt/tp1/ex2.php?message=msg1&size=15&color=%23ff0000">Afficher msg1</a>
-<a href="/web-dyna-alt/tp1/ex2.php?message=msg2&size=30&color=%2308ff00">Afficher msg2</a>
-<a href="/web-dyna-alt/tp1/ex2.php?message=msg3&size=50&color=%230088ff">Afficher msg3</a>';
+  	</h3>
 
+	<form method="GET">
 
-if (empty($message) or empty($size) or empty($color)){
+		<label for="message">Message : </label>
+		<input type="text" value="Bonjour" name="message" id="message">
 
-    echo '/!\ Pas de message fournie /!\ ';
+		<label for="size">Size : </label>
+		<input type="number" value="12" name="size" id="size">
 
-} else {
+		<label for="color">Color : </label>
+		<input type="color" name="color" id="color">
 
-    echo $msg;
+		<input type="submit" value="Valider">
+	</form>
+</body>
 
-}
-
-echo $form;
-
-echo $link;
+</html> 
